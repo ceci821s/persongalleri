1bc5656807c98198e337b73bb9260d6dc9655456
Cecilie Dinsen <ceci821s@stud.kea.dk>
2022-02-07 10:02:56 +0100

    kun vises troende

--- script.js
+++ script.js
@@ -6,27 +6,37 @@ const options = {
   },
 };
 
+const section = document.querySelector("section");
+const template = document.querySelector("template").content;
+
+document.addEventListener("DOMContentLoaded", start);
+let personer;
+let filter = "ja";
+function start() {
+  loadJSON();
+}
+
 async function hentData() {
   const resspons = await fetch(url, options);
   //   console.log(resspons);
+
   const json = await resspons.json();
   vis(json);
 }
 
-const section = document.querySelector("section");
-const template = document.querySelector("template").content;
-
 function vis(personliste) {
   console.log(personliste);
   personliste.forEach((medlem) => {
-    const klon = template.cloneNode(true);
-    klon.querySelector(
-      "h2"
-    ).textContent = `${medlem.fornavn} ${medlem.efternavn}`;
-    klon.querySelector(".email").textContent = medlem.email;
-    // klon.querySelector(".info").textContent = person.info;
-    klon.querySelector("img").src = "/faces/" + medlem.profilbillede;
-    section.appendChild(klon);
+    if (filter == medlem.troende) {
+      const klon = template.cloneNode(true);
+      klon.querySelector(
+        "h2"
+      ).textContent = `${medlem.fornavn} ${medlem.efternavn}`;
+      klon.querySelector(".email").textContent = medlem.email;
+      // klon.querySelector(".info").textContent = person.info;
+      klon.querySelector("img").src = "/faces/" + medlem.profilbillede;
+      section.appendChild(klon);
+    }
   });
 }
 
